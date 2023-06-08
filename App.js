7c604afcf5ffff678aebad6491211d3c977a7b46
waymaymay <Rdvickywei@gmail.com>
2023-06-08 18:55:02 +0800

    first commit

---
+++ App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="body">
+      <header className="App-header">
+        {/* <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a> */}
+
+        <div className='start'>Hello Leave</div>
+        <button>請確認</button>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
