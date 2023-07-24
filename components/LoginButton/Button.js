332fadf90a96a7409f11cae4b3022727fab51234
waymaymay <Rdvickywei@gmail.com>
2023-07-24 21:59:55 +0800

    update button-class

--- components/LoginButton/Button.js
+++ components/LoginButton/Button.js
@@ -1,10 +1,10 @@
 // Button.js
 
 import React from 'react';
-import './Button.css';
+import'./Button';
 
 const Button = ({ label }) => {
-  return <button className="custom-button">{label}</button>;
+  return <button className="Login-Button">{label}</button>;
 };
 
 export default Button;
