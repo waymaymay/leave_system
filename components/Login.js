df68b2dfd5bc121696bf464c3355a1b6f7a84b3f
waymaymay <Rdvickywei@gmail.com>
2023-07-25 15:25:05 +0800

    set up login page 分為左右邊 右邊為圖 左邊為登入按鈕

--- components/Login.js
+++ components/Login.js
@@ -11,9 +11,22 @@ const Login = () => {
 
   return (
     <div className="login-container">
-      <h2>請假登入</h2>
-      {/* Add your login form here */}
-      <Button label="登入" onClick={handleLogin} />
+        <div className="left-section">
+            <h2>請假登入</h2>
+            {/* Add login form here */}
+            <Button label="登入" onClick={handleLogin} />
+        </div>
+        
+        <div className="right-section">
+             {/* Content for the right section */}
+            <img src="https://assets.blog.engoo.com/wp-content/uploads/sites/3/2022/09/07094921/dayoff-1024x597.png.webp"
+            width="600vmin" height="400vmin" alt='picture'/>
+
+        </div>
+            
+            
+            
+         
     </div>
   );
 };
